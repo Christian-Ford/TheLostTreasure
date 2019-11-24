94fdd88a4f13fe4b3ab5ab632d27514e56361feb
Christian-Ford <fjord.christian@gmail.com>
2019-11-24 00:50:02 -0600

    Added get and drop to player methods

--- player.py
+++ player.py
@@ -1,14 +1,14 @@
 # Creating our player
 
 class Player:
-    def __init__(self, name, current_room, health, money, weapon, armor, inventory):
+    def __init__(self, name, current_room, health, money, weapon, armor):
         self.name = name
         self.current_room = current_room
         self.health = health
         self.money = money
         self.weapon = weapon
         self.armor = armor
-        self.inventory = inventory
+        self.inventory = []
 
     # Player Movement
     def move_player(self, command):
@@ -17,10 +17,19 @@ class Player:
         else:
             print(f'YOU CANNOT GO THAT WAY')
 
+    # Define Get and Drop Items
+    def get_item(self, pickup_item):
+        self.inventory.append(pickup_item)
+
+    def drop_item(self, drop_item):
+        self.inventory.remove(drop_item)
+        self.current_room.inventory.append(drop_item)
+
     #Output to console
     def __str__(self):
         output = ''
         output += '---------------------------------------- \n'
+        output += f'Player Name: {self.name} \n'
         output += f'Current Room: {self.current_room} \n'
 
         return output
